2eee1cb2c75db9041f3ce621384d869422adb5ec
TIRTH39 <130666953+TIRTH39@users.noreply.github.com>
2023-04-13 18:12:37 +0530

    Add files via upload

---
+++ lec.c2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	printf(" --------\n");	
+	printf("|\t|\n");
+	printf("t\t|\n");
+	printf("i\t|\n");
+	printf("r\t|\n");
+	printf("t\t|\n");
+	printf("h\t|\n");
+	printf("|\t|\n");
+	printf(" --------\n");
+}
---
+++ lec.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+	
+     printf("---------\n");
+     printf("|\t|\n");
+	 printf("|\t|\n");
+     printf("| tirth |\n");
+	 printf("|\t|\n");
+	 printf("|\t|\n");
+	 printf("|\t|\n");
+	 printf("---------\n");
+}
